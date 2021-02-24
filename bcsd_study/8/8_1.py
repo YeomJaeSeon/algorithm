62bc596af41d7402d07fa2528df8e6cd9392fc5c
YeomJaeSeon <a89541457@gmail.com>
2021-02-24 13:26:13 +0900

    트리 순회 solve

--- bcsd_study/8/8_1.py
+++ bcsd_study/8/8_1.py
@@ -6,6 +6,10 @@
 # 1991번
 
 # 재귀함수로 풀어야할듯 딱 느낌이옴
+# 입력되는 데이터도 많지않아서 구현에만 신경쓰면 될거같다
+
+import sys
+sys.setrecursionlimit(3000)
 
 N = int(input())
 
@@ -14,10 +18,57 @@ for _ in range(N):
   tree.append(list(input().split()))
 
 visited = []
-def front(tree):
-  first = start[0] 
-  second = frstart[1]
-  third = start[2]
+frontTree = []
 
-  visited.append(first)
+for t in tree:
+  frontTree.append(t[0])
 
+def search(nodes):
+  if nodes[0] != '.':
+    visited.append(nodes[0])
+
+  if nodes[1] != '.':
+    leftIndex = frontTree.index(nodes[1])
+    search(tree[leftIndex])
+  if nodes[2] != '.':
+    rightIndex = frontTree.index(nodes[2])
+    search(tree[rightIndex])
+
+search(tree[0])
+for node in visited:
+  print(node, end='')
+print('')
+
+visited=[]
+
+def search2(nodes):
+  if nodes[1] != '.':
+    leftIndex = frontTree.index(nodes[1])
+    search2(tree[leftIndex])
+  if nodes[0] != '.':
+    visited.append(nodes[0])
+  if nodes[2] != '.':
+    rightIndex = frontTree.index(nodes[2])
+    search2(tree[rightIndex])
+
+search2(tree[0])
+for node in visited:
+  print(node, end='')
+print('')
+
+visited = []
+
+def search3(nodes):
+  if nodes[1] != '.':
+    leftIndex = frontTree.index(nodes[1])
+    search3(tree[leftIndex])
+  if nodes[2] != '.':
+    rightIndex = frontTree.index(nodes[2])
+    search3(tree[rightIndex])
+  if nodes[0] != '.':
+    visited.append(nodes[0])
+
+search3(tree[0])
+for node in visited:
+  print(node, end='')
+print('')
