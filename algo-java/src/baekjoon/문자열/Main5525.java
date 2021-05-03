c9f5d807e407e67a0c658861dddf16ddbc1a5357
YeomJaeSeon <a89541457@gmail.com>
2021-05-03 17:51:08 +0900

    5525 solve 1062 시간초과

--- algo-java/src/baekjoon/문자열/Main5525.java
+++ algo-java/src/baekjoon/문자열/Main5525.java
@@ -10,26 +10,30 @@ public class Main5525 {
         int N = Integer.parseInt(br.readLine());
         int s_len = Integer.parseInt(br.readLine());
         String s = br.readLine();
-        String pn = "";
 
-        for(int i = 1; i <= 2 * N + 1; i++){
-            if(i % 2 == 1) pn += "I";
-            else pn += "O";
-        }
-
+        int cnt = 0;
         int answer = 0;
 
-        // substring으로 똮딲 나눔
-//        for(int i = 0; i < s_len - (2 * N + 1); i++){
-//            String checkStr = s.substring(i, i + (2 * N + 1));
-//            if(checkStr.equals(pn)) answer++;
-//            if(!s.substring(i + 1).contains(pn)) {
-////                System.out.println("s.substring(i + (2 * N + 1)) = " + s.substring(i + (2 * N + 1)));
-////                System.out.println("i = " + i);
-//                break;
-//            }
-//        }
+        for(int i = 0; i < s_len - 2; i++){
+            if(s.charAt(i) == 'I' && s.charAt(i + 1) == 'O' && s.charAt(i + 2) == 'I'){
+                ++cnt;
+                i++;
+                if(cnt == N){
+                    answer++;
+                    cnt--;
+                }
+            }else{
+                cnt = 0;
+            }
+        }
 
         System.out.println(answer);
     }
 }
+
+/**
+ 시간초과 : Pn을만들려고했음 만드는 과정 자체가 시간초과를 유발함
+ 물론 O(N * M)으로 하나씩 비교해도 시간초과나지만 이문제는 Pn만드는 과정자체가 시간초과남
+ PN만들필요없이 입력되는 N값을 이용하면됨
+ 연속되는 패턴의 문자열이므로 단순히 O(N)으로 비교함
+ **/
