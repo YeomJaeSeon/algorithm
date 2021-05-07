1b5786e6668d14bc3914bdf193768a6e5ae2f534
YeomJaeSeon <a89541457@gmail.com>
2021-05-07 16:25:20 +0900

    1967 solve HashMap -> Node class의 객체로 변경했더니 메모리 초과 해결.

--- algo-java/src/baekjoon/그래프탐색/Main1967.java
+++ algo-java/src/baekjoon/그래프탐색/Main1967.java
@@ -5,9 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+
 public class Main1967 {
     static boolean[] visited;
-    static Map<Integer, Integer>[] graph;
+    static List<Node>[] graph;
     static int max = 0; // 트리 전체의 max
     static int sum = 0; // 합저장할변수
     static int subMax = 0; // 노드 하나의 max
@@ -26,28 +27,26 @@ public class Main1967 {
         }
 
         visited = new boolean[n + 1]; // 10,000 * 1 = 10,000
-        graph = new Map[n + 1]; // 10,000 * 24 = 240,000
+        graph = new ArrayList[n + 1]; // 10,000 * 24 = 240,000
 
         for(int i = 0; i < graph.length; i++){
-            graph[i] = new HashMap<>();
+            graph[i] = new ArrayList<>();
         }
 
         for(int i = 1; i < n + 1; i++){
             for(int j = 0; j < inputArr.length; j++){
                 if(inputArr[j][0] > i) break; // 부모 노드는 항상 자식노드보다 작다.
                 if(inputArr[j][0] == i){
-                    graph[i].put(inputArr[j][1], inputArr[j][2]);
+                    graph[i].add(new Node(inputArr[j][1], inputArr[j][2]));
                 }
                 if(inputArr[j][1] == i){
-                    graph[i].put(inputArr[j][0], inputArr[j][2]);
+                    graph[i].add(new Node(inputArr[j][0], inputArr[j][2]));
                 }
             }
         }
 
-        System.out.println(Arrays.toString(graph));
+//        System.out.println(Arrays.toString(graph));
 
-        // graph : [Map, Map, Map ... ]
-        // Map key : 연결된 node, value : 간선의 가중치
         for(int i = 1; i < n + 1; i++){
             dfs(i);
             max = Math.max(max, subMax);
@@ -55,24 +54,55 @@ public class Main1967 {
             Arrays.fill(visited, false);
         }
         System.out.println(max);
+
     }
     public static void dfs(int startNode){
         visited[startNode] = true;
 
-        Iterator<Map.Entry<Integer, Integer>> iterator = graph[startNode].entrySet().iterator();
+        Iterator<Node> iterator = graph[startNode].iterator();
 
         // key는 연결된 node, value 가중치
         while(iterator.hasNext()){
-            Map.Entry<Integer, Integer> next = iterator.next();
-            int nextNode = next.getKey();
-            int gravity = next.getValue();
-            if(!visited[nextNode]){
+            Node next = iterator.next();
+            int nextNode = next.num; // 다음노드
+            int gravity = next.gravity; // 다음노드까지의 가중치
+            if(!visited[nextNode]){ // 방문한적이 없으면
                 sum += gravity;
                 dfs(nextNode);
                 subMax = Math.max(sum, subMax);
-                sum -= gravity;
+                sum -= gravity; // 여긴 빼야함. 더이상 갈 노드가없는 노드는 이제 재귀함수 끝나서 이 문으로 오면 최근의 가중치를 빼야함 
+                // 예를들면 1 -> 2-> 4 -> 7로 왔는데 7에서 dfs재귀함수이제끝나면 최근 가중치 7을 뺴고 다시 노드 4에서 방문하지않은 8번 노드로 간다. 
+                // 이러한 흐름으로 품
             }
         }
-
     }
 }
+
+class Node{
+    int num;
+    int gravity;
+    Node(int num, int gravity){
+        this.num = num;
+        this.gravity = gravity;
+    }
+
+//    @Override
+//    public String toString() {
+//        return "node : " + num +", gravity : " + gravity;
+//    }
+}
+
+/**
+ 메모리초과
+ - 인접리스트행렬의 데이터 (노드 번호와 가중치)를 HashMap으로 key value로 저장해서 했더니 메모리초과났음
+ 
+ 해결
+ - 단순히 Node클래스 만들어서 노드번호, 가중치 저장하는 멤버변수 만들어서 했더니 메모리초과해결.;
+ - hashMap은 해쉬테이블때문에(배열 + LinkedList) 메모리를 많이잡아먹는건가? - 정확히는모르겠음 왜그런지
+ 
+ 풀이
+ 1. dfs 를 이용함
+ 2. 어쨌든 각 노드에서 어떤 노드를 가던간에 최대값을 알아야함 그래서 한 노드 (예를들면 노드 1에서 dfs 그래프 탐색한다면)
+ 1에서 모든 노드까지 갈떄의 최대 subMax를 구함
+ 3. 노드1 ~ 노드 N까찌의 max를 subMax를 통해 갱신하며 구함
+ **/
