44ac7e1018d884f7a834795d6ad9e29f23922c6b
YeomJaeSeon <a89541457@gmail.com>
2021-07-05 10:29:00 +0900

    solve 5430 - String, 1343 - greedy

--- algo-java/src/baekjoon/그리디/Main1343.java
+++ algo-java/src/baekjoon/그리디/Main1343.java
@@ -1,67 +1,59 @@
 package baekjoon.그리디;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main1343 {
     static String board;
-    static List<String> list = new ArrayList<>();
 
     //AAAA, BB 2가지
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         board = br.readLine();
-        String str = "";
 
-        for(int i = 0; i < board.length(); i++){
-            if(board.charAt(i) == '.'){
-                int length = str.length();
-                if(length == 0) continue;
-                if(length % 2 != 0){
-                    System.out.println(-1);
-                    return;
-                }
-                StringBuilder sb = new StringBuilder();
-
-                while(length >= 4){
-                    sb.append("AAAA");
-                    length -= 4;
-                }
-                if(length == 2){
-                    sb.append("BB");
-                }
-                list.add(sb.toString());
-                str = "";
-                continue;
-            }
-            str += board.charAt(i);
-        }
-
-        if(str.length() > 0 && str.length() % 2 != 0){
-            System.out.println(-1);
-        }else{
-            StringBuilder sb = new StringBuilder();
-            int length = str.length();
-            while(length >= 4){
-                sb.append("AAAA");
-                length -= 4;
-            }
-            if(length == 2){
-                sb.append("BB");
-            }
-            list.add(sb.toString());
-        }
-        String result = String.join("", list);
         StringBuilder sb = new StringBuilder();
-        int idx = 0;
+        StringBuilder Xsb = new StringBuilder();
         for(int i = 0; i < board.length(); i++){
             if(board.charAt(i) == '.'){
-                sb.append(".");
+                int length = Xsb.length();
+                if(length > 0){
+                    if(length % 2 != 0){
+                        System.out.println(-1);
+                        return;
+                    }
+                    while(length >= 4){
+                        sb.append("AAAA");
+                        length -= 4;
+                    }
+                    if(length == 2){
+                        sb.append("BB");
+                    }
+                }
+                sb.append('.');
+                Xsb = new StringBuilder();
             }else{
-                sb.append(result.charAt(idx++));
+                Xsb.append(board.charAt(i));
             }
+        }
+        int length = Xsb.length();
+        if(length % 2 != 0){
+            System.out.println(-1);
+            return;
+        }
+        while(length >= 4){
+            sb.append("AAAA");
+            length -= 4;
+        }
+        if(length == 2){
+            sb.append("BB");
         }
         System.out.println(sb.toString());
+
     }
 }
+
+/**
+ * 문제 자체는 어렵지않음, 2로나누어떨어지지않으면 가능하고
+ * 4보다 크면 AAAA만 계쏙채우다가
+ * 2남을면 BB채우면 되는문제인데
+ * 문자열 조절해서 좀 틀렸음.
+ */
