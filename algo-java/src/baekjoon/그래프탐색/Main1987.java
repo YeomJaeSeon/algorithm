47286687d5e9a4abe67ab9515489cf7821ecac18
YeomJaeSeon <a89541457@gmail.com>
2021-08-10 23:07:58 +0900

    solve 1987

--- algo-java/src/baekjoon/그래프탐색/Main1987.java
+++ algo-java/src/baekjoon/그래프탐색/Main1987.java
@@ -3,53 +3,46 @@ package baekjoon.그래프탐색;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main1987 {
-    static int R, C; //세로 가로
-    static char[][] board;
+    static int R, C;
+    static int[][] board;
     static boolean[] visited = new boolean[26];
-    static final int ASCII = 65;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, -1, 0, 1};
-    static Stack<Character> stack = new Stack<>();
     static int max = -1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        board = new char[R][C];
 
-        for (int i = 0; i < R; i++) {
+        board = new int[R][C];
+        for(int i = 0; i < R; i++){
             String str = br.readLine();
-            for (int j = 0; j < C; j++) {
-                board[i][j] = str.charAt(j);
+            for(int j = 0; j < C; j++){
+                board[i][j] = str.charAt(j) - 65;
             }
         }
-
         dfs(0, 0, 0);
+
         System.out.println(max + 1);
+
     }
     static void dfs(int x, int y, int count){
+        visited[board[x][y]] = true;
         max = Math.max(max, count);
-        visited[(int)board[x][y] - ASCII] = true;
-        stack.push(board[x][y]);
-        board[x][y] = (char)(Integer.parseInt(String.valueOf(count)) + 1 + 48);
-
         for(int i = 0; i < 4; i++){
             int nextX = x + dx[i];
             int nextY = y + dy[i];
+
             if(nextX < 0 || nextX >= R || nextY < 0 || nextY >= C) continue;
-            if((int)board[nextX][nextY] < (int)'A' || (int)'Z' < (int)board[nextX][nextY]) continue; // 대문자 알파벳이 아닐경우
-            if(visited[(int)board[nextX][nextY] - ASCII]) continue; // 이미 방문한 대문자 알파벳일경우
+            if(visited[board[nextX][nextY]]) continue;
 
             dfs(nextX, nextY, count + 1);
         }
-        board[x][y] = stack.pop(); //원상복귀
-        visited[(int)board[x][y] - ASCII] = false;
+        visited[board[x][y]] = false;
     }
 }
