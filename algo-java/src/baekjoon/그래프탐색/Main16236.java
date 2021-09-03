2c65e4ad2ab9b6b5690f16f99588c0030ec51b9f
YeomJaeSeon <a89541457@gmail.com>
2021-09-03 16:36:51 +0900

    solve 16236

--- algo-java/src/baekjoon/그래프탐색/Main16236.java
+++ algo-java/src/baekjoon/그래프탐색/Main16236.java
@@ -3,21 +3,16 @@ package baekjoon.그래프탐색;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main16236 {
     static int N;
     static int[][] board;
-    static int sharkX;
-    static int sharkY;
-    static int fishCount;
-    static int[] dx = {-1, 0, 0, 1};
-    static int[] dy = {0, -1, 1, 0};
+    static int startX, startY;
     static boolean[][] visited;
-
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+    static List<BabyShark> feedList = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
@@ -27,103 +22,101 @@ public class Main16236 {
         for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             for(int j = 0; j < N; j++){
-                int num = Integer.parseInt(st.nextToken());
-                if(num == 9){
-                    sharkX = i;
-                    sharkY = j;
-                }else if(num == 0){
-                    board[i][j] = num;
-                }
-                else{
-                    fishCount++;
-                    board[i][j] = num;
+                int value = Integer.parseInt(st.nextToken());
+                if(value == 9){
+                    startX = i;
+                    startY = j;
+                }else{
+                    board[i][j] = value;
                 }
             }
         }
 
-        if(fishCount == 0){
-            System.out.println(0);
-        }else{
-            System.out.println(bfs());
-        }
 
-
+        System.out.println(bfs(startX, startY));
     }
-    static int bfs(){
-        Queue<Shark> q = new LinkedList<>();
-        q.offer(new Shark(sharkX, sharkY, 0, 0, 2));
-        visited[sharkX][sharkY] = true;
-        int totalMoveCount = 0;
+    static int bfs(int x, int y){
+        int babySharkSize = 2;
+        int eatCount = 0;
+        Queue<BabyShark> q = new LinkedList<>();
+        visited[x][y] = true;
+        q.offer(new BabyShark(x, y, 0));
+        int totalTimes = 0;
 
-        while(!q.isEmpty()){
-            if(fishCount == 0) break;
-            Shark current = q.poll();
-            for(int i = 0; i < 4; i++){
-                int nextX = current.x + dx[i];
-                int nextY = current.y + dy[i];
+        while(true){
+            while(!q.isEmpty()){
+                BabyShark current = q.poll();
+                for(int i = 0; i < 4; i++){
+                    int nextX = current.x + dx[i];
+                    int nextY = current.y + dy[i];
 
-                if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= N) continue;
-                if(board[nextX][nextY] > current.size) continue;
-                if(!visited[nextX][nextY] && board[nextX][nextY] != 0 && board[nextX][nextY] < current.size){
-                    System.out.println("먹었다");
-                    System.out.println("nextX : " + nextX + " nextY : " + nextY);
-                    q.clear();
-                    fishCount--;
-                    totalMoveCount += (current.moveCount + 1);
-                    if(current.eatCount + 1 == current.size){
-                        q.offer(new Shark(nextX, nextY, 0, 0, current.size + 1));
-                    }else{
-                        q.offer(new Shark(nextX, nextY, 0, current.eatCount + 1, current.size));
+                    if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= N) continue;
+                    if(!visited[nextX][nextY] && (board[nextX][nextY] == 0 || board[nextX][nextY] == babySharkSize)){
+                        q.offer(new BabyShark(nextX, nextY, current.times + 1));
+                        visited[nextX][nextY] = true;
+                        continue;
                     }
-                    initVisited();
-                    visited[nextX][nextY] = true;
-                    board[nextX][nextY] = 0;
-                    break;
-                }
-                if(!visited[nextX][nextY] && board[nextX][nextY] == current.size){
-                    q.offer(new Shark(nextX, nextY, current.moveCount + 1, current.eatCount, current.size));
-                    visited[nextX][nextY] = true;
-                    continue;
-                }
-                if(!visited[nextX][nextY] && board[nextX][nextY] == 0){
-                    q.offer(new Shark(nextX, nextY, current.moveCount + 1, current.eatCount, current.size));
-                    visited[nextX][nextY] = true;
-                    continue;
+                    if(!visited[nextX][nextY] && board[nextX][nextY] != 0 && board[nextX][nextY] < babySharkSize){
+                        //can eat
+                        feedList.add(new BabyShark(nextX, nextY, current.times + 1));
+                        q.offer(new BabyShark(nextX, nextY, current.times + 1));
+                        visited[nextX][nextY] = true;
+                        continue;
+                    }
                 }
             }
+            if(!feedList.isEmpty()){
+                Collections.sort(feedList, (o1, o2) -> {
+                    if(o1.times == o2.times){
+                        if(o1.x == o2.x){
+                            return o1.y - o2.y;
+                        }else{
+                            return o1.x - o2.x;
+                        }
+                    }else{
+                        return o1.times - o2.times;
+                    }
+                });
+                //먹을수있는 먹이중 가장 가까우면서 위쪽 그리고 왼쪽에있는 먹이 찾기
+                BabyShark feed = feedList.get(0); //먹을 먹이
+
+                eatCount++;
+                if(eatCount == babySharkSize){
+                    babySharkSize++;
+                    eatCount = 0;
+                }
+                totalTimes += feed.times;
+
+                feedList.clear(); //먹이 리스트 비워주기
+                visited = new boolean[N][N]; // 방문 처리 초기화
+                visited[feed.x][feed.y] = true; //(처음 탐색 시작지점)
+                board[feed.x][feed.y] = 0; // 먹었으니 0으로 바꾸어줌
+                q.offer(new BabyShark(feed.x, feed.y, 0));
+            }else{
+                break;
+            }
         }
-        return totalMoveCount;
-    }
-    static void initVisited(){
-        for (boolean[] booleans : visited) {
-            Arrays.fill(booleans, false);
-        }
+        return totalTimes;
     }
 }
-class Shark implements Comparable<Shark>{
+
+class BabyShark{
     int x;
     int y;
-    int moveCount;
-    int eatCount;
-    int size;
+    int times;
 
-    public Shark(int x, int y, int moveCount, int eatCount, int size) {
+    public BabyShark(int x, int y, int times) {
         this.x = x;
         this.y = y;
-        this.moveCount = moveCount;
-        this.eatCount = eatCount;
-        this.size = size;
+        this.times = times;
     }
 
     @Override
-    public int compareTo(Shark o) {
-        if(y != o.y){
-            return y - o.y;
-        }else{
-            return x - o.x;
-        }
+    public String toString() {
+        return "BabyShark{" +
+                "x=" + x +
+                ", y=" + y +
+                ", times=" + times +
+                '}';
     }
 }
-
-// 예제 4번 자꾸틀림
-// 잡을수있는 물고기중 가장 위, 왼쪽에있는 물고기를 잡을해보자.
--- algo-java/src/baekjoon/그래프탐색/Main16236_2.java
+++ algo-java/src/baekjoon/그래프탐색/Main16236_2.java
@@ -1,4 +1,121 @@
 package baekjoon.그래프탐색;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main16236_2 {
+    static int N;
+    static int[][] board;
+    static int startX, startY;
+    static boolean[][] visited;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+    static List<Shark> feedList = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        board = new int[N][N];
+        visited = new boolean[N][N];
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0; j < N; j++){
+                int value = Integer.parseInt(st.nextToken());
+                if(value == 9){
+                    startX = i;
+                    startY = j;
+                }else{
+                    board[i][j] = value;
+                }
+            }
+        }
+
+        System.out.println(bfs(startX, startY));
+
+    }
+    static int bfs(int startX, int startY){
+        int sharkSize = 2;
+        int eatCount = 0;
+        Queue<Shark> q = new LinkedList<>();
+        q.offer(new Shark(startX, startY, 0));
+        visited[startX][startY] = true;
+        int totalCount = 0;
+
+        while(true){
+            while(!q.isEmpty()){
+                Shark current = q.poll();
+                for(int i = 0; i < 4; i++){
+                    int nextX = current.x + dx[i];
+                    int nextY = current.y + dy[i];
+
+                    if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= N) continue;
+                    if(!visited[nextX][nextY] && board[nextX][nextY] != 0 && sharkSize > board[nextX][nextY]){
+                        visited[nextX][nextY] = true;
+                        q.offer(new Shark(nextX, nextY, current.moveCount + 1));
+                        feedList.add(new Shark(nextX, nextY, current.moveCount + 1));
+                        continue;
+                    }
+                    if(!visited[nextX][nextY] && (board[nextX][nextY] == 0 || board[nextX][nextY] == sharkSize)){
+                        visited[nextX][nextY] = true;
+                        q.offer(new Shark(nextX, nextY, current.moveCount + 1));
+                        continue;
+                    }
+                }
+            }
+
+            if(!feedList.isEmpty()){
+                Collections.sort(feedList, (o1, o2) -> {
+                    if(o1.moveCount == o2.moveCount){
+                        if(o1.x == o2.x){
+                            return o1.y - o2.y;
+                        }else{
+                            return o1.x - o2.x;
+                        }
+                    }else{
+                        return o1.moveCount - o2.moveCount;
+                    }
+                });
+
+                Shark feed = feedList.get(0);
+                totalCount += feed.moveCount;
+                eatCount++;
+                if(eatCount == sharkSize){
+                    sharkSize++;
+                    eatCount = 0;
+                }
+                feedList = new ArrayList<>();
+                visited = new boolean[N][N];
+                q.offer(new Shark(feed.x, feed.y, 0));
+                visited[feed.x][feed.y] = true;
+                board[feed.x][feed.y] = 0;
+            }else{
+                break;
+            }
+        }
+        return totalCount;
+    }
+}
+
+class Shark{
+    int x;
+    int y;
+    int moveCount;
+
+    public Shark(int x, int y, int moveCount) {
+        this.x = x;
+        this.y = y;
+        this.moveCount = moveCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Shark{" +
+                "x=" + x +
+                ", y=" + y +
+                ", moveCount=" + moveCount +
+                '}';
+    }
 }
