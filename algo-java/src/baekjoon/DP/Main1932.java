64ec806740ed9b318063e8a00304e2f6defc3fb4
YeomJaeSeon <a89541457@gmail.com>
2021-09-07 19:22:34 +0900

    solve 1932

--- algo-java/src/baekjoon/DP/Main1932.java
+++ algo-java/src/baekjoon/DP/Main1932.java
@@ -11,23 +11,45 @@ import java.util.StringTokenizer;
 public class Main1932 {
     static int N;
     static List<List<Integer>> triangle = new ArrayList<>();
+    static List<List<Integer>> dp = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         for(int i = 0; i < N; i++){
             triangle.add(new ArrayList<>());
+            dp.add(new ArrayList<>());
         }
         for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             for(int j = 0; j <= i; j++){
                 triangle.get(i).add(Integer.parseInt(st.nextToken()));
+                dp.get(i).add(-1);
             }
         }
 
-//        for (List<Integer> integers : triangle) {
-//            Collections.fill(integers, -1);
-//        }
+        dp.get(0).set(0, triangle.get(0).get(0));
 
+        for(int i = 0; i < N - 1; i++){
+            for(int j = 0; j < dp.get(i).size(); j++){
+                if(dp.get(i + 1).get(j) == -1){
+                    dp.get(i + 1).set(j, dp.get(i).get(j) + triangle.get(i + 1).get(j));
+                }else{
+                    dp.get(i + 1).set(j,
+                            Math.max(dp.get(i + 1).get(j), dp.get(i).get(j) + triangle.get(i + 1).get(j))
+                    );
+                }
+                if(dp.get(i + 1).get(j + 1) == -1){
+                    dp.get(i + 1).set(j + 1, dp.get(i).get(j) + triangle.get(i + 1).get(j + 1));
+                }else{
+                    dp.get(i + 1).set(j + 1,
+                                Math.max(dp.get(i + 1).get(j + 1), dp.get(i).get(j) + triangle.get(i + 1).get(j + 1))
+                            );
+                }
+            }
+        }
+
+        
+        System.out.println(Collections.max(dp.get(N - 1)));
 
     }
 }
