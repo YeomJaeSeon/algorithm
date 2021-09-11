526c9142227bc48143c208307aa829737bbf8761
YeomJaeSeon <a89541457@gmail.com>
2021-09-11 13:12:06 +0900

    solve 2193, try to solve 14501

---
+++ algo-java/src/baekjoon/DP/Main14501.java
@@ -0,0 +1,54 @@
+package baekjoon.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Customer{
+    int t;
+    int p;
+    int d;
+
+    Customer(int t, int p, int d){
+        this.t = t;
+        this.p = p;
+        this.d = d;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "t=" + t +
+                ", p=" + p +
+                ", d=" + d +
+                '}';
+    }
+}
+
+public class Main14501 {
+    static int N;
+    static List<Customer> list = new ArrayList<>();
+    static int[] dp = new int[16];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i += 1){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int t = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+            if(i + t > N) continue;
+            list.add(new Customer(t, p, i + 1));
+        }
+
+        dp[1] = list.stream().filter(i -> i.d == 1).mapToInt(i -> i.p);
+
+        System.out.println(list);
+
+
+    }
+}
--- algo-java/src/baekjoon/DP/Main2193.java
+++ algo-java/src/baekjoon/DP/Main2193.java
@@ -3,45 +3,22 @@ package baekjoon.DP;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
 
 public class Main2193 {
     static int n;
-    static List<List<Integer>> dp = new ArrayList<>();
+    static long[] dp = new long[91];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i <= 90; i++){
-            dp.add(new ArrayList<>());
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 2;
+
+        for(int i = 4; i <= n; i++){
+            dp[i] = dp[i - 2] + dp[i - 1];
         }
 
-        dp.get(0).add(1);
-        dp.get(1).add(1);
-
-        dp.get(2).add(1);
-        dp.get(2).add(1);
-
-        dp.get(3).add(1);
-        dp.get(3).add(2);
-
-        for(int i = 4; i <= 90; i++){
-            dp.get(i).add(1);
-            for(int j = 1; j < dp.get(i - 1).size(); j++){
-                dp.get(i).add(dp.get(i - 1).get(j) + 1);
-            }
-            if(i % 2 == 0){
-                dp.get(i).add(1);
-            }
-        }
-
-        for (List<Integer> integers : dp) {
-            System.out.println(integers);
-            System.out.println();
-        }
-
-        System.out.println("sum : " + dp.get(n - 1).stream().mapToInt(i -> i).sum());
+        System.out.println(dp[n]);
     }
 }
