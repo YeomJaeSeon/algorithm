fe410388be9cd73918d1620b9c56c3552750fa94
YeomJaeSeon <a89541457@gmail.com>
2021-10-05 18:02:34 +0900

    backtracking training 15651 15652

---
+++ algo-java/src/training/backtracking/Main15652.java
@@ -0,0 +1,40 @@
+package training.backtracking;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main15652 {
+    static int N, M;
+    static int[] result;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        result = new int[M];
+
+        recursive(0);
+
+        bw.flush();
+        bw.close();
+    }
+    static void recursive(int m) throws IOException {
+        if(m == M){
+            for(int i = 0; i < M; i++){
+                bw.write(result[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for(int i = (m == 0) ? 0 : result[m - 1] - 1; i < N; i++){
+            result[m] = i + 1;
+            recursive(m + 1);
+        }
+
+    }
+}
