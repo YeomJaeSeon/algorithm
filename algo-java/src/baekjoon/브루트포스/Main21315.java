b93eaa34044fc1c0bf9ff587dc8b3f89cd87e60f
YeomJaeSeon <a89541457@gmail.com>
2022-01-11 18:04:04 +0900

    solve 1463(동적계획법)

--- algo-java/src/baekjoon/브루트포스/Main21315.java
+++ algo-java/src/baekjoon/브루트포스/Main21315.java
@@ -21,10 +21,12 @@ public class Main21315 {
         for(int i = 0; i < N ; i++){
             result[i] = Integer.parseInt(st.nextToken());
         }
+        int max = baseLog(N, 2);
+        System.out.println(max);
 
         //k모든경우
-        for(int i = 1; i <= 9; i++){
-            for(int j = 1; j <= 9; j++){
+        for(int i = 1; i <= max; i++){
+            for(int j = 1; j <= max; j++){
                 if(playGame(i, j)){
                     a = i;
                     b = j;
@@ -36,24 +38,46 @@ public class Main21315 {
         System.out.println(a + " " + b);
     }
     static boolean playGame(int first, int second){
-        //first -> second
+        // first
         int[] arr = new int[N];
         for(int i = 0; i < N; i++){
             arr[i] = i + 1;
         }
 
         int value = (int)Math.pow(2, first);
-        int point = N - 1;
+        int part1Start = 0;
+        int part1End = (N - 1) - value;
+        int part2Start = (N - 1) - value + 1;
+        int part2End = N - 1;
 
         while(value >= 1){
             int[] tmp = new int[N];
-            int start = point - (value - 1);
-            for(int i = 0; i < value; i++){
-                tmp[i] = arr[start + i];
+
+            for(int i = 0; i < N; i++){
+                tmp[i] = arr[i];
             }
 
+            // part1 <-> part2 swap
+            for(int i = part2Start; i <= part2End; i++){
+                tmp[i - (part1End - part1Start + 1)] = arr[i];
+            }
+            for(int i = part1Start; i <= part1End; i++){
+                tmp[i + (part2End - part2Start + 1)] = arr[i];
+            }
+
+            arr = tmp;
+            value = (int)Math.pow(2, --first);
+            part1Start = 0;
+            part1End = value - 1;
+//            part2Start = (N - 1) - value + 1;
+//            part2End = N - 1;
         }
 
+        return true;
+        // second
+    }
+    static int baseLog(int x, int base){
+        return (int) (Math.log10(x) / Math.log10(base));
     }
 
 }
